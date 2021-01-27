a2a74625feb6531cce47bd2c513b93156a4e1ef0
Barry Williams <barry.williams485@schools.sa.edu.au>
2021-01-27 22:30:01 +1030

    rebase

--- code.js
+++ code.js
@@ -5,23 +5,29 @@ var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'my-game',
 // declare global variables for game
 var player;
 var arrowKey;
-let canjump = true;
 var sky, mountains, city;
 
 // preload game assets - runs once at start
 function preload() {
-  game.load.spritesheet('dude', 'dude.png', 32, 48); 
-  game.load.image ('city', 'city-skyline.png');
-  game.load.image ('mountains', 'mountain-skyline.png'); 
-  game.load.image('sky', 'sky-clouds.jpg');
-  game.load.image('platform-50')
-
-
+  game.load.spritesheet('dude', 'assets/images/dude.png', 32, 48); 
+  game.load.image ('city', 'assets/images/city-skyline.png');
+  game.load.image ('mountains', 'assets/images/mountain-skyline.png'); 
+  game.load.image('sky', 'assets/images/sky-clouds.jpg');
+  game.load.image('platform-50', 'assets/images/platform-050w.png');
+  game.load.image('platform-100', 'assets/images/platform-100w.png');
+  game.load.image('platform-200', 'assets/images/platform-200w.png');
+  game.load.image('platform-300', 'assets/images/platform-300w.png');
+  game.load.image('platform-400', 'assets/images/platform-400w.png');
+  game.load.image('platform-500', 'assets/images/platform-500w.png');
+  game.load.image('wall-50', 'assets/images/wall-050h.png');
+  game.load.image('wall-150', 'assets/images/wall-150h.png');
+  game.load.image('wall-250', 'assets/images/wall-250h.png');
 }
 
 // create game world - runs once after "preload" finished
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.checkCollision.up = false;
   sky = game.add.tileSprite(0, 0, 1000, 600, 'sky');
   mountains = game.add.tileSprite(0, 0, 1000, 600, 'mountains');
   city = game.add.tileSprite(0, 0, 1000, 600, 'city');
@@ -42,10 +48,41 @@ function create() {
   //animations
   player.animations.add('left', [0, 1, 2, 3], 10, true);
   player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+  // PLATFORMS
+  platformGroup = game.add.group();
+  platformGroup.enableBody = true;
+
+  // add ground platform
+  var ground = platformGroup.create(0, game.world.height - 25, 'platform-500');
+  ground.scale.setTo(10, 1); // 10 * 500 = 5000 pixels wide
+  // add platforms
+  platformGroup.create(200, 500, 'platform-100');
+  platformGroup.create(400, 425, 'platform-100');
+  platformGroup.create(600, 350, 'platform-100');
+  platformGroup.create(50, 100, 'platform-50');
+  platformGroup.create(250, 175, 'platform-50');
+  platformGroup.create(450, 260, 'platform-50');
+  platformGroup.create(900, 275, 'platform-200');
+  platformGroup.create(1150, 475, 'platform-50');
+  platformGroup.create(1350, 500, 'platform-50');
+  platformGroup.setAll('body.immovable', true);
+  //wall platforms
+  wallGroup = game.add.group();
+  wallGroup.enableBody = true;
+  wallGroup.create(525, 525, 'wall-50');
+  wallGroup.create(1000, 425, 'wall-150');
+  wallGroup.create(2000, 525, 'wall-50');
+  wallGroup.create(3000, 525, 'wall-50');
+  wallGroup.create(4000, 425, 'wall-50');
+  wallGroup.setAll('body.immovable', true);
+  
 }
 
 // update gameplay - runs in continuous loop after "create" finished
 function update() {
+  game.physics.arcade.collide(player, platformGroup);
+  game.physics.arcade.collide(player, wallGroup);
   if (arrowKey.left.isDown) {
     player.body.velocity.x = -200;
     player.animations.play('left');
@@ -59,12 +96,8 @@ function update() {
     player.frame = 4;
   }
 
-  if (arrowKey.up.isDown && canjump) {
+  if (arrowKey.up.justDown && player.body.touching.down) {
     player.body.velocity.y = -300;
-    canjump = false;
-  }
-  if (player.body.velocity.y == 0){
-    canjump = true;
   }
   // BACKGROUND PARALLAX    
   sky.tilePosition.x = game.camera.x * -0.2;
